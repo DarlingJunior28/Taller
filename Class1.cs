decebdac8ac14bec34e89da95f9389214b9191a4
DarlingJunior28 <150557907+DarlingJunior28@users.noreply.github.com>
2024-07-22 22:39:06 -0400

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TallerLINQ
+{
+    public class Class1
+    {
+        static void Main()
+        {
+            // Definir una lista de números
+            List<int> numeros = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+};
+            // Consulta LINQ para obtener números pares
+            var numerosPares = from numero in numeros
+                               where numero % 2 == 0
+                               select numero;
+            // Mostrar resultados
+            Console.WriteLine("Números Pares:");
+            foreach (var numero in numerosPares)
+            {
+                Console.WriteLine(numero);
+            }
+        }
+    }
+}
---
+++ Estudiantes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Estudiantes
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public int Edad { get; set; }
+
+    public static void Main()
+    {
+        List<Estudiantes> estudiantes = new List<Estudiantes>
+        {
+            new Estudiantes { Id = 1, Nombre = "Juan", Edad = 20 },
+            new Estudiantes { Id = 2, Nombre = "Carlos", Edad = 22 },
+            new Estudiantes { Id = 3, Nombre = "Manuel", Edad = 19 },
+            new Estudiantes { Id = 4, Nombre = "Perla", Edad = 21 }
+        };
+
+        var estudiantesOrdenados = from estudiante in estudiantes
+                                   orderby estudiante.Nombre
+                                   select estudiante;
+
+        Console.WriteLine("\nEstudiantes Ordenados por Nombre:");
+        foreach (var estudiante in estudiantesOrdenados)
+        {
+            Console.WriteLine(estudiante.Nombre);
+        }
+    }
+}
